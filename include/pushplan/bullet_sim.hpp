95802400d7edd1c33d1630192f3e043f8a29e309
Dhruv Mauria Saxena <dhruvmsaxena@gmail.com>
2021-08-06 11:53:02 -0400

    pybullet simulator integration

---
+++ include/pushplan/bullet_sim.hpp
@@ -0,0 +1,76 @@
+#ifndef BULLET_SIM_HPP
+#define BULLET_SIM_HPP
+
+#include <sensor_msgs/JointState.h>
+#include <ros/ros.h>
+#include <Eigen/Eigen>
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <random>
+
+namespace clutter
+{
+
+class BulletSim
+{
+public:
+	BulletSim(
+		const std::string& tables, bool ycb=false,
+		int replay_id=-1, const std::string& suffix=std::string(),
+		int immovable_objs=5, int movable_objs=5);
+
+	bool SetRobotState(const sensor_msgs::JointState& msg);
+	bool ResetArm(const int& arm);
+	bool CheckScene(const int& arm, int& count);
+	bool ResetScene();
+	bool SetColours();
+
+private:
+	int m_num_immov, m_num_mov, m_robot_id, m_tables;
+
+	ros::NodeHandle m_nh;
+	std::vector<ros::ServiceClient> m_services;
+	std::unordered_map<std::string, int> m_servicemap;
+
+	std::vector<double> m_robot;
+	std::vector<std::vector<double>> m_immov, m_mov, m_removed;
+	std::vector<std::pair<int, int>> m_immov_ids, m_mov_ids, m_removed_ids;
+
+	std::random_device m_dev;
+	std::mt19937 m_rng;
+	std::uniform_real_distribution<double> m_distD;
+	std::uniform_int_distribution<> m_distI;
+
+	bool setupObjects(bool ycb);
+	bool setupObjectsFromFile(
+		bool ycb, int replay_id, const std::string& suffix);
+
+	bool addRobot(int replay_id, const std::string& suffix);
+
+	bool setupPrimitiveObjectsFromFile(
+		int replay_id, const std::string& suffix);
+	bool setupYCBObjectsFromFile(
+		int replay_id, const std::string& suffix);
+	void setupTableFromFile(int replay_id, const std::string& suffix);
+	bool setupPrimitiveObjects();
+	bool setupYCBObjects();
+
+	void readRobotFromFile(int replay_id, const std::string& suffix);
+
+	bool setupTables();
+	bool readTables(const std::string& filename);
+
+	bool resetSimulation();
+	void setupServices();
+
+	bool removeObject(const int& id);
+	double getObjectCoord(double origin, double half_extent, double border);
+	std::string getPartialFilename(int id);
+};
+
+} // namespace clutter
+
+
+#endif // BULLET_SIM_HPP
---
+++ scripts/sim.py
@@ -0,0 +1,507 @@
+#!/usr/bin/env python3
+import numpy as np
+import os, glob, random # for random textures to objects
+import time
+import collections
+import multiprocessing as mp
+import copy
+
+import rospy
+import pybullet as p
+import pybullet_data
+from pybullet_utils import bullet_client as bc
+
+from pushplan.srv import AddObject, AddObjectResponse
+from pushplan.srv import AddYCBObject, AddYCBObjectResponse
+from pushplan.srv import ResetSimulation, ResetSimulationResponse
+from pushplan.srv import AddRobot, AddRobotResponse
+from pushplan.srv import SetRobotState, SetRobotStateResponse
+from pushplan.srv import ResetArm, ResetArmResponse
+from pushplan.srv import CheckScene, CheckSceneResponse
+from pushplan.srv import ResetScene, ResetSceneResponse, ResetSceneRequest
+from pushplan.srv import SetColours, SetColoursResponse
+
+from utils import *
+
+class BulletSim:
+	def __init__(self, gui, shadows=False):
+		rospy.init_node('BulletSim')
+
+		self.sims = []
+		self.sim_datas = {}
+		self.fridge = rospy.get_param("/fridge", False)
+
+		self.table_rgba = list(np.random.rand(3)) + [1]
+		self.table_specular = list(np.random.rand(3))
+
+		self.camera_set = False
+
+		if gui:
+			self.cpu_count = 1
+
+			sim, info = self.setupSim(True, shadows)
+			self.sims.append(sim)
+			self.sim_datas[0] = info
+		else:
+			cpus = rospy.get_param("sim/cpus", 0)
+			if cpus <= 0:
+				self.cpu_count = mp.cpu_count() - 1
+			else:
+				self.cpu_count = min(mp.cpu_count() - 1, cpus)
+
+			for i in range(self.cpu_count):
+				sim, info = self.setupSim(False, shadows)
+				self.sims.append(sim)
+				self.sim_datas[i] = info
+
+		self.add_object = rospy.Service('add_object',
+										AddObject, self.AddObject)
+		self.add_robot = rospy.Service('add_robot',
+										AddRobot, self.AddRobot)
+		self.add_ycb_object = rospy.Service('add_ycb_object',
+										AddYCBObject, self.AddYCBObject)
+		self.set_robot_state = rospy.Service('set_robot_state',
+										SetRobotState, self.SetRobotStateAll)
+		self.reset_arm = rospy.Service('reset_arm',
+										ResetArm, self.ResetArmAll)
+		self.check_scene = rospy.Service('check_scene',
+										CheckScene, self.CheckScene)
+		self.reset_scene = rospy.Service('reset_scene',
+										ResetScene, self.ResetScene)
+		self.set_colours = rospy.Service('set_colours',
+										SetColours, self.SetColours)
+		self.reset_simulation = rospy.Service('reset_simulation',
+										ResetSimulation, self.ResetSimulation)
+
+		self.ResetSimulation(-1)
+
+	def ResetSimulation(self, req):
+		for i, sim in enumerate(self.sims):
+			sim.resetSimulation()
+			sim.setGravity(0,0,-9.81)
+			self.sim_datas[i]['ground_plane_id'] = sim.loadURDF("plane.urdf")
+			sim.setRealTimeSimulation(0)
+
+			if not self.fridge:
+				self.sim_datas[i]['table_id'] = [1]
+			else:
+				self.sim_datas[i]['table_id'] = [1, 2, 3, 4, 5]
+			self.sim_datas[i]['robot_id'] = -1
+			self.sim_datas[i]['objs'] = {}
+			self.sim_datas[i]['num_objs'] = 0
+
+		return ResetSimulationResponse(True)
+
+	def AddObject(self, req):
+		sim_id = req.sim_id
+		all_sims = sim_id < 0
+
+		for i, sim in enumerate(self.sims) if all_sims else enumerate(self.sims[sim_id:sim_id + 1]):
+			sim_idx = i if all_sims else sim_id
+			sim_data = self.sim_datas[sim_idx]
+
+			if (req.shape == 0):
+				obj_id = self.addBox(req, sim_idx)
+			elif (req.shape == 2):
+				obj_id = self.addCylinder(req, sim_idx)
+
+			aabb = sim.getAABB(obj_id, -1)
+			lower_limits = [x - CLEARANCE for x in aabb[0]]
+			upper_limits = [x + CLEARANCE for x in aabb[1]]
+
+			ground_plane_id = sim_data['ground_plane_id']
+			table_id = sim_data['table_id']
+
+			overlaps = sim.getOverlappingObjects(lower_limits, upper_limits)
+			if (obj_id <= table_id[-1]): # part of table or fridge
+				intersects = [oid != ground_plane_id and oid not in table_id and oid != obj_id for (oid, link) in overlaps]
+			else:
+				intersects = [oid != ground_plane_id and oid != table_id[0] and oid != obj_id for (oid, link) in overlaps]
+			if any(intersects): # only necessary overlaps: table and itself
+				sim.removeBody(obj_id)
+				del sim_data['objs'][obj_id]
+				obj_id = -1
+
+			if obj_id != -1:
+				friction_min = rospy.get_param("objects/friction_min", 0.8)
+				friction_max = rospy.get_param("objects/friction_max", 1.2)
+				mu = np.random.uniform(friction_min, friction_max) if req.mu < 0 else req.mu
+				if obj_id in table_id:
+					mu = friction_min
+
+				sim.changeDynamics(obj_id, -1, lateralFriction=mu)
+				sim_data['objs'][obj_id]['mu'] = mu
+				sim_data['objs'][obj_id]['movable'] = req.movable
+				sim_data['num_objs'] += 1
+
+		return AddObjectResponse(obj_id, [])
+
+	def AddYCBObject(self, req):
+		sim_id = req.sim_id
+		all_sims = sim_id < 0
+
+		for i, sim in enumerate(self.sims) if all_sims else enumerate(self.sims[sim_id:sim_id + 1]):
+			sim_idx = i if all_sims else sim_id
+			sim = self.sims[sim_idx]
+			sim_data = self.sim_datas[sim_idx]
+
+			obj_id = req.obj_id
+			xyz = [req.o_x, req.o_y, req.o_z]
+			rpy = [req.o_r, req.o_p, req.o_yaw]
+
+			filename = os.path.dirname(os.path.abspath(__file__)) + '/'
+			filename += '../env/ycb/{object}/urdf/{object}_tsdf.urdf'.format(object=YCB_OBJECTS[obj_id])
+
+			body_id = sim.loadURDF(
+							fileName=filename,
+							basePosition=xyz,
+							baseOrientation=sim.getQuaternionFromEuler(rpy))
+
+			sim_data['objs'][body_id] = {
+								'shape': obj_id,
+								'xyz': xyz,
+								'rpy': rpy,
+								'ycb': True,
+							}
+
+			aabb = sim.getAABB(body_id, -1)
+			lower_limits = [x - CLEARANCE for x in aabb[0]]
+			upper_limits = [x + CLEARANCE for x in aabb[1]]
+
+			ground_plane_id = sim_data['ground_plane_id']
+			table_id = sim_data['table_id']
+
+			overlaps = sim.getOverlappingObjects(lower_limits, upper_limits)
+			if (obj_id <= table_id[-1]):
+				intersects = [oid != ground_plane_id and oid not in table_id and oid != body_id for (oid, link) in overlaps]
+			else:
+				intersects = [oid != ground_plane_id and oid != table_id[0] and oid != body_id for (oid, link) in overlaps]
+			if any(intersects): # only necessary overlaps: table and itself
+				sim.removeBody(body_id)
+				del sim_data['objs'][body_id]
+				body_id = -1
+
+			if body_id != -1:
+				friction_min = rospy.get_param("objects/friction_min", 0.8)
+				friction_max = rospy.get_param("objects/friction_max", 1.2)
+				mu = np.random.uniform(friction_min, friction_max)
+
+				sim.changeDynamics(body_id, -1, lateralFriction=mu)
+				sim_data['objs'][body_id]['mu'] = mu
+				sim_data['objs'][body_id]['movable'] = req.movable
+				sim_data['num_objs'] += 1
+
+		return AddYCBObjectResponse(body_id, [])
+
+
+	def AddRobot(self, req):
+		sim_id = req.sim_id
+		all_sims = sim_id < 0
+
+		for i, sim in enumerate(self.sims) if all_sims else enumerate(self.sims[sim_id:sim_id + 1]):
+			sim_idx = i if all_sims else sim_id
+			sim_data = self.sim_datas[sim_idx]
+
+			xyz = [req.x, req.y, req.z]
+			quat = [req.qx, req.qy, req.qz, req.qw]
+
+			urdf_path = os.path.dirname(os.path.abspath(__file__)) + '/' + PR2_URDF
+			robot_id = sim.loadURDF(urdf_path,
+								basePosition=xyz,
+								baseOrientation=quat,
+								useFixedBase=1)
+
+			sim_data['robot_id'] = robot_id
+			sim_data['robot'] = {
+							'xyz': xyz,
+							'quat': quat,
+						}
+			sim_data['joint_idxs'] = joints_from_names(robot_id, PR2_GROUPS['right_arm'], sim=self.sims[sim_idx])
+
+		return AddRobotResponse(self.sim_datas[0]['robot_id'])
+
+	def SetRobotStateAll(self, req):
+		for i, sim in enumerate(self.sims):
+			sim_id = i
+
+			sim = self.sims[sim_id]
+			sim_data = self.sim_datas[sim_id]
+			robot_id = sim_data['robot_id']
+
+			self.disableCollisionsWithObjects(sim_id)
+
+			sim_data['start_joint_names'] = req.state.name
+
+			joint_idxs = joints_from_names(robot_id, sim_data['start_joint_names'], sim=sim)
+			for jidx, jval in zip(joint_idxs, req.state.position):
+				sim.resetJointState(robot_id, jidx, jval)
+			sim.stepSimulation()
+
+			gripper_joints = joints_from_names(robot_id, PR2_GROUPS['right_gripper'], sim=sim)
+			for gjidx in gripper_joints:
+				sim.resetJointState(robot_id, gjidx, 0.0)
+			sim.stepSimulation()
+
+			self.enableCollisionsWithObjects(sim_id)
+
+		return SetRobotStateResponse(True)
+
+	def ResetArmAll(self, req):
+		for i, sim in enumerate(self.sims):
+			sim_id = i
+
+			sim = self.sims[sim_id]
+
+			robot_id = self.sim_datas[sim_id]['robot_id']
+
+			self.disableCollisionsWithObjects(sim_id)
+
+			if req.arm == 0:
+				joint_idxs = joints_from_names(robot_id, PR2_GROUPS['left_arm'], sim=sim)
+				joint_config = REST_LEFT_ARM
+			else:
+				joint_idxs = joints_from_names(robot_id, PR2_GROUPS['right_arm'], sim=sim)
+				joint_config = WIDE_RIGHT_ARM
+
+			for jidx, jval in zip(joint_idxs, joint_config):
+				sim.resetJointState(robot_id, jidx, jval)
+			sim.stepSimulation()
+
+			self.enableCollisionsWithObjects(sim_id)
+
+		return ResetArmResponse(True)
+
+	def CheckScene(self, req):
+		sim_id = req.sim_id
+		all_sims = sim_id < 0
+		intersects = []
+
+		for i, sim in enumerate(self.sims) if all_sims else enumerate(self.sims[sim_id:sim_id + 1]):
+			intersects = list(set(intersects))
+
+			sim_idx = i if all_sims else sim_id
+			sim = self.sims[sim_idx]
+			sim_data = self.sim_datas[sim_idx]
+
+			table_id = sim_data['table_id']
+			robot_id = sim_data['robot_id']
+			ground_plane_id = sim_data['ground_plane_id']
+
+			if req.arm == 0:
+				joint_idx = joint_from_name(robot_id, 'l_elbow_flex_joint', sim=sim)
+			else:
+				joint_idx = joint_from_name(robot_id, 'r_elbow_flex_joint', sim=sim)
+
+			aabb = get_subtree_aabb(robot_id, root_link=joint_idx, sim=sim)
+			lower_limits = [x - CLEARANCE for x in aabb[0]]
+			upper_limits = [x + CLEARANCE for x in aabb[1]]
+
+			overlaps = sim.getOverlappingObjects(lower_limits, upper_limits)
+			for (obj_id, link) in overlaps:
+				if (obj_id != ground_plane_id) and (obj_id not in table_id) and (obj_id != robot_id):
+					sim.removeBody(obj_id)
+					del sim_data['objs'][obj_id]
+					sim_data['num_objs'] -= 1
+					intersects.append(obj_id)
+
+		intersects = list(set(intersects))
+		return CheckSceneResponse(intersects)
+
+	def ResetScene(self, req, sim_id_in=None):
+		if sim_id_in is None:
+			sim_id = req.sim_id
+			all_sims = sim_id < 0
+		else:
+			sim_id = sim_id_in
+			all_sims = False
+
+		for i, sim in enumerate(self.sims) if all_sims else enumerate(self.sims[sim_id:sim_id + 1]):
+			sim_idx = i if all_sims else sim_id
+			sim = self.sims[sim_idx]
+			sim_data = self.sim_datas[sim_idx]
+
+			table_id = sim_data['table_id']
+
+			if (not self.camera_set):
+				table_xyz = copy.deepcopy(sim_data['objs'][table_id[0]]['xyz'])
+				table_xyz[2] += 0.4
+				sim.resetDebugVisualizerCamera(
+					cameraDistance=1.0, cameraYaw=-60.0, cameraPitch=-10.0,
+					cameraTargetPosition=table_xyz)
+				self.camera_set = True
+
+			self.disableCollisionsWithObjects(sim_idx)
+
+			for obj_id in sim_data['objs']:
+				xyz = sim_data['objs'][obj_id]['xyz']
+				rpy = sim_data['objs'][obj_id]['rpy']
+				sim.resetBasePositionAndOrientation(obj_id,
+											posObj=xyz,
+											ornObj=sim.getQuaternionFromEuler(rpy))
+
+			self.enableCollisionsWithObjects(sim_idx)
+
+		return ResetSceneResponse(True)
+
+
+	def SetColours(self, req):
+		sim_id = req.sim_id
+		all_sims = sim_id < 0
+
+		for i, sim in enumerate(self.sims) if all_sims else enumerate(self.sims[sim_id:sim_id + 1]):
+			sim_idx = i if all_sims else sim_id
+			sim = self.sims[sim_idx]
+			sim_data = self.sim_datas[sim_idx]
+
+			for idx, obj_id in enumerate(req.ids):
+				if req.type[idx] == -1: # table
+					sim.changeVisualShape(obj_id, -1,
+									rgbaColor=TABLE_COLOUR + [1.0],
+									specularColor=TABLE_COLOUR)
+					sim_data['objs'][obj_id]['type'] = -1
+				if req.type[idx] == 0: # static
+					sim.changeVisualShape(obj_id, -1,
+									rgbaColor=STATIC_COLOUR + [1.0],
+									specularColor=STATIC_COLOUR)
+					sim_data['objs'][obj_id]['type'] = 0
+				if req.type[idx] == 1: # moveable
+					sim.changeVisualShape(obj_id, -1,
+									rgbaColor=MOVEABLE_COLOUR + [1.0],
+									specularColor=MOVEABLE_COLOUR)
+					sim_data['objs'][obj_id]['type'] = 1
+
+		return SetColoursResponse(True)
+
+
+	def disableCollisionsWithObjects(self, sim_id):
+		sim = self.sims[sim_id]
+		sim_data = self.sim_datas[sim_id]
+
+		robot_id = sim_data['robot_id']
+		for joint in get_joints(robot_id, sim=sim):
+			for obj_id in sim_data['objs']:
+				sim.setCollisionFilterPair(robot_id, obj_id, joint, -1,
+											enableCollision=0)
+
+	def enableCollisionsWithObjects(self, sim_id):
+		sim = self.sims[sim_id]
+		sim_data = self.sim_datas[sim_id]
+
+		robot_id = sim_data['robot_id']
+		for joint in get_joints(robot_id, sim=sim):
+			for obj_id in sim_data['objs']:
+				sim.setCollisionFilterPair(robot_id, obj_id, joint, -1,
+											enableCollision=1)
+
+	def addBox(self, req, sim_id):
+		sim = self.sims[sim_id]
+		sim_data = self.sim_datas[sim_id]
+
+		xyz = [req.o_x, req.o_y, req.o_z]
+		rpy = [req.o_r, req.o_p, req.o_yaw]
+		half_extents = [req.x_size, req.y_size, req.z_size]
+		mass = 0 if req.locked else np.random.rand() * 0.5
+		mass = mass if req.mass <= 0 else req.mass
+
+		if sim_data['num_objs'] < len(sim_data['table_id']):
+			rgba = self.table_rgba
+			specular = self.table_specular
+		else:
+			rgba = list(np.random.rand(3)) + [1]
+			specular = list(np.random.rand(3))
+
+		vis_id = sim.createVisualShape(shapeType=sim.GEOM_BOX,
+								halfExtents=half_extents,
+								rgbaColor=rgba,
+								specularColor=specular)
+		coll_id = sim.createCollisionShape(shapeType=sim.GEOM_BOX,
+								halfExtents=half_extents)
+		body_id = sim.createMultiBody(baseMass=mass,
+								baseCollisionShapeIndex=coll_id,
+								baseVisualShapeIndex=vis_id,
+								basePosition=xyz,
+								baseOrientation=sim.getQuaternionFromEuler(rpy),
+								baseInertialFramePosition=[0, 0, 0],
+								baseInertialFrameOrientation=[0, 0, 0, 1])
+
+		sim_data['objs'][body_id] = {
+								'shape': req.shape,
+								'vis': vis_id,
+								'coll': coll_id,
+								'mass': mass,
+								'xyz': xyz,
+								'rpy': rpy,
+								'extents': [req.x_size, req.y_size, req.z_size],
+								'ycb': False,
+							}
+		return body_id
+
+	def addCylinder(self, req, sim_id):
+		sim = self.sims[sim_id]
+		sim_data = self.sim_datas[sim_id]
+
+		xyz = [req.o_x, req.o_y, req.o_z]
+		rpy = [req.o_r, req.o_p, req.o_yaw]
+		radius = max([req.x_size, req.y_size])
+		height = req.z_size
+		mass = 0 if req.locked else np.random.rand() * 0.5
+		mass = mass if req.mass <= 0 else req.mass
+
+		vis_id = sim.createVisualShape(shapeType=sim.GEOM_CYLINDER,
+								radius=radius,
+								length=height,
+								rgbaColor=list(np.random.rand(3)) + [1],
+								specularColor=list(np.random.rand(3)))
+		coll_id = sim.createCollisionShape(shapeType=sim.GEOM_CYLINDER,
+								radius=radius,
+								height=height)
+		body_id = sim.createMultiBody(baseMass=mass,
+								baseCollisionShapeIndex=coll_id,
+								baseVisualShapeIndex=vis_id,
+								basePosition=xyz,
+								baseOrientation=sim.getQuaternionFromEuler(rpy),
+								baseInertialFramePosition=[0, 0, 0],
+								baseInertialFrameOrientation=[0, 0, 0, 1])
+
+		sim_data['objs'][body_id] = {
+								'shape': req.shape,
+								'vis': vis_id,
+								'coll': coll_id,
+								'mass': mass,
+								'xyz': xyz,
+								'rpy': rpy,
+								'extents': [req.x_size, req.y_size, req.z_size],
+								'ycb': False,
+							}
+		return body_id
+
+	def setupSim(self, gui, shadows):
+		connection = p.GUI if gui else p.DIRECT
+		sim = bc.BulletClient(connection_mode=connection)
+		sim.setAdditionalSearchPath(pybullet_data.getDataPath())
+		sim.setGravity(0,0,-9.81)
+		ground_plane_id = sim.loadURDF("plane.urdf")
+		sim.setRealTimeSimulation(0)
+		info = {'ground_plane_id': ground_plane_id}
+
+		sim.configureDebugVisualizer(sim.COV_ENABLE_GUI, False)
+		sim.configureDebugVisualizer(sim.COV_ENABLE_TINY_RENDERER, True)
+		sim.configureDebugVisualizer(sim.COV_ENABLE_RGB_BUFFER_PREVIEW, False)
+		sim.configureDebugVisualizer(sim.COV_ENABLE_DEPTH_BUFFER_PREVIEW, False)
+		sim.configureDebugVisualizer(sim.COV_ENABLE_SEGMENTATION_MARK_PREVIEW, False)
+		sim.configureDebugVisualizer(sim.COV_ENABLE_SHADOWS, shadows)
+		sim.configureDebugVisualizer(sim.COV_ENABLE_WIREFRAME, True)
+
+		if not self.fridge:
+			info['table_id'] = [1]
+		else:
+			info['table_id'] = [1, 2, 3, 4, 5]
+		info['robot_id'] = -1
+		info['objs'] = {}
+		info['num_objs'] = 0
+
+		return sim, info
+
+if __name__ == '__main__':
+	sim = BulletSim(GUI)
+	rospy.spin()
