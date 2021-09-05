fc2ba56bdd9b9d8fd7a2e5945515ea5c9a38cdb4
Dhruv Mauria Saxena <dhruvmsaxena@gmail.com>
2021-09-05 17:05:42 -0400

    run through file with planning scenes

--- test/coop_plan.cpp
+++ test/coop_plan.cpp
@@ -19,52 +19,67 @@ int main(int argc, char** argv)
 	// Let publishers set up
 	ros::Duration(1.0).sleep();
 
-	// put together scene file name
-	std::string scene, level;
-	ph.param<std::string>("scene_id", scene, "100000");
-	int scene_id = std::stoi(scene);
-	if (scene_id < 100000) {
-		level = "0";
-		ROS_WARN("Planning for a scene with no movable objects!");
-	}
-	else if (scene_id < 200000) {
-		level = "5";
-	}
-	else if (scene_id < 300000) {
-		level = "10";
-	}
-	else {
-		level = "15";
-	}
+	// read from NONE file
+	std::string filename(__FILE__);
+	auto found = filename.find_last_of("/\\");
+	filename = filename.substr(0, found + 1) + "../dat/FIRST.txt";
 
-	std::string planfile(__FILE__);
-	auto found = planfile.find_last_of("/\\");
-	planfile = planfile.substr(0, found + 1) + "../../../../simplan/src/simplan/data/clutter_scenes/";
-	planfile += level + "/plan_" + scene + "_SCENE.txt";
+	std::ifstream NONE;
+	NONE.open(filename);
 
-	Planner p(planfile, scene_id);
-
-	while (true)
+	if (NONE.is_open())
 	{
-		p.Plan();
-		bool rearrange = false;
-		// bool rearrange = p.Rearrange();
-		std::uint32_t violation = p.RunSim();
-		ROS_INFO("Iteration result: rearrangement planning = %d, execution = %d", rearrange, violation);
+		std::string line, level;
+		while (!NONE.eof())
+		{
+			getline(NONE, line);
+			if (line.length() == 0) {
+				break;
+			}
+			int scene_id = std::stoi(line);
+			if (scene_id < 100000)
+			{
+				level = "0";
+				ROS_WARN("Planning for a scene with no movable objects!");
+			}
+			else if (scene_id < 200000) {
+				level = "5";
+			}
+			else if (scene_id < 300000) {
+				level = "10";
+			}
+			else {
+				level = "15";
+			}
 
-		if (!rearrange && violation > 0) {
-			ROS_ERROR("FAILURE!!!");
-			break;
-		}
+			std::string planfile(__FILE__);
+			auto found = planfile.find_last_of("/\\");
+			planfile = planfile.substr(0, found + 1) + "../../../../simplan/src/simplan/data/clutter_scenes/";
+			planfile += level + "/plan_" + line + "_SCENE.txt";
+			ROS_WARN("Run planner on: %s", planfile.c_str());
 
-		if (violation == 0) {
-			ROS_INFO("SUCCESS!!!");
-			break;
+			Planner p(planfile, scene_id);
+
+			p.Plan();
+			bool rearrange = p.Rearrange();
+			std::uint32_t violation = p.RunSim();
+			ROS_INFO("Iteration result: rearrangement planning = %d, execution = %d", rearrange, violation);
+
+			if (violation == 0) {
+				ROS_WARN("SUCCESS!!!");
+			}
+			else {
+				ROS_WARN("FAILURE!!!");
+			}
 		}
 	}
+	else
+	{
+		ROS_ERROR("Planner init error");
+		return false;
+	}
 
-	p.AnimateSolution();
-	ros::spin();
+	NONE.close();
 
 	return 0;
 }
