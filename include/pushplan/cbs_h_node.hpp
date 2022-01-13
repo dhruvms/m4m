760cc67f1208d96dce965249ad084a9f057410d1
Dhruv Mauria Saxena <dhruvmsaxena@gmail.com>
2022-01-13 16:29:53 -0500

    cbs related structs

---
+++ include/pushplan/cbs_h_node.hpp
@@ -0,0 +1,40 @@
+#ifndef CBS_H_NODE_HPP
+#define CBS_H_NODE_HPP
+
+#include <pushplan/conflicts.hpp>
+
+#include <boost/heap/fibonacci_heap.hpp>
+
+#include <list>
+#include <memory>
+
+namespace clutter
+{
+
+struct HighLevelNode
+{
+	struct HeapCompare {
+		bool operator()(
+				const HighLevelNode& s, const HighLevelNode& t) const
+		{
+			return s.m_g < t.m_g;
+		}
+	};
+
+	std::list<std::shared_ptr<Constraint> > m_constraints; // constraints to be satisfied (parent + 1)
+	std::list<std::shared_ptr<Conflict> > m_conflicts; // conflicts at this node
+	Conflict m_conflict; // selected conflict
+	std::vector<std::pair<int, Trajectory*> > m_solution; // agent solutions
+
+	int m_g, m_depth, m_makespan, m_generate, m_expand;
+	boost::heap::fibonacci_heap<HighLevelNode, boost::heap::compare<HighLevelNode::HeapCompare> >::handle_type m_OPEN_h;
+
+	HighLevelNode* m_parent;
+	std::vector<HighLevelNode*> m_children;
+	int m_replanned;
+};
+
+} // namespace clutter
+
+
+#endif // CBS_H_NODE_HPP
---
+++ include/pushplan/conflicts.hpp
@@ -0,0 +1,49 @@
+#ifndef CONFLICTS_HPP
+#define CONFLICTS_HPP
+
+#include <pushplan/types.hpp>
+
+#include <memory>
+
+namespace clutter
+{
+
+struct Constraint
+{
+	int m_me, m_other, m_time;
+	LatticeState m_q;
+};
+
+struct Conflict
+{
+	int m_a1, m_a2;
+	int m_t;
+	LatticeState m_q1, m_q2;
+	std::shared_ptr<Constraint> m_c1, m_c2;
+
+	void InitConflict(int a1, int a2, int t, LatticeState q1, LatticeState q2, bool robot=false)
+	{
+		m_a1 = a1;
+		m_a2 = a2;
+		m_t = t;
+		m_q1 = q1;
+		m_q2 = q2;
+
+		m_c1 = std::make_shared<Constraint>();
+		m_c1->m_me = m_a1;
+		m_c1->m_other = m_a2;
+		m_c1->m_time = m_t;
+		m_c1->m_q = q2;
+
+		m_c2 = std::make_shared<Constraint>();
+		m_c2->m_me = m_a2;
+		m_c2->m_other = robot ? m_a2 : m_a1;
+		m_c2->m_time = m_t;
+		m_c2->m_q = robot ? q2 : q1;
+	}
+};
+
+} // namespace clutter
+
+
+#endif // CONFLICTS_HPP
