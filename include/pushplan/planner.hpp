a3af3f806bd9f94803bf08608888b0f441ea936d
Dhruv Mauria Saxena <dhruvmsaxena@gmail.com>
2022-01-28 11:55:28 -0500

    helpers and passthroughs

--- include/pushplan/planner.hpp
+++ include/pushplan/planner.hpp
@@ -41,6 +41,29 @@ public:
 		return m_agents.at(m_agent_map[id]).get();
 	}
 
+	std::vector<Agent*> GetAllAgents(){
+		std::vector<Agent*> all_agents;
+		for(int i = 0; i < m_agents.size(); i++){
+			all_agents.push_back(m_agents[i].get());
+		}
+		return all_agents;
+	}
+
+	const std::vector<Object>* Get2DRobot(const LatticeState& s) {
+		return m_robot->GetObject(s);
+	}
+	const std::vector<Object>* GetObject(int id) {
+		return m_agents.at(m_agent_map[id]).GetObject();
+	};
+
+	bool CheckRobotCollision(Agent* a, const LatticeState& robot_state, int t, bool process)
+	{
+		if (!process) {
+			return m_robot->CheckCollision(robot_state, t);
+		}
+		return m_robot->CheckCollisionWithObject(robot_state, a, t);
+	}
+
 private:
 	std::string m_scene_file;
 	std::shared_ptr<CollisionChecker> m_cc;
